f571050b9f01044de56dce639406e2d81ad5b2a7
vv258 <vv258@cornell.edu>
2018-12-20 04:48:57 -0500

    v6

--- PiInterface.c
+++ PiInterface.c
@@ -89,7 +89,7 @@ int iSampleCh[4],iSampleChBuf[4];
 int iTotalNumOfADCSamples=0, iNumOfADCSamples[4];
 
 int iBuffer[4][1024];
-
+char ReceiveBuf[256];
 void printLine(int line_number, char* print_buffer, short text_color, short back_color){
     // line number 0 to 31 
     /// !!! assumes tft_setRotation(0);
@@ -246,6 +246,11 @@ void vExec_Read_Buf(char *cRecvData){
     iNumOfSamples=((cSampleMSBbits & 0x3f)<<6)|(cSampleLSBbits & 0x3f);
     sprintf(cbuffer,"Read %d Samples from Buffer Number %d", iNumOfSamples,cBufferNum);
     printLine(8, cbuffer, ILI9340_WHITE, ILI9340_BLUE); 
+    
+    
+
+
+
      for(iByteCount=0;iByteCount<iNumOfSamples;iByteCount++){
         while(!UARTTransmitterIsReady(UART2));
         UARTSendDataByte(UART2, iBuffer[cBufferNum][iByteCount]);
@@ -271,29 +276,31 @@ void vExec_Write_Buf(char *cRecvData){
     sprintf(cbuffer,"Write %d Samples to Buffer Number %d", iNumOfSamples,cBufferNum);
     printLine(8, cbuffer, ILI9340_WHITE, ILI9340_BLUE); 
     
-   
-    for(iByteCount=0;iByteCount<iNumOfSamples;iByteCount++){
-            while(!UARTReceivedDataIsAvailable(UART2));
-            iBuffer[cBufferNum][iByteCount]=UARTGetDataByte(UART2);
-    }
+   	// set the transfer parameters: source & destination address, source & destination size, number of bytes per event
+    // Setting the last parameter to one makes the DMA output one byte/interrupt
+    DmaChnSetTxfer(DMA_CHANNEL2, (void*)&U2RXREG, iBuffer[cBufferNum], iNumOfSamples*2, 1, 1 );
+
+	// set the transfer event control: what event is to start the DMA transfer
+    // In this case, timer2
+	DmaChnSetEventControl(DMA_CHANNEL2, DMA_EV_START_IRQ(_UART2_RX_IRQ));
+    DmaChnOpen(DMA_CHANNEL2, 0, DMA_OPEN_DEFAULT);
+    
+    
 }
 
 void vExec_Set_PWM_Per(char *cRecvData){
     char cPeriodMSBbits= cRecvData[0];
-    char cPeriodMLSBbits =cRecvData[1];
-    char cPeriodLSBbits =cRecvData[2];
-    int iPeriod;
+    char cPeriodLSBbits =cRecvData[1];
+    unsigned int iPeriod;
     tft_fillScreen(ILI9340_BLACK);
     sprintf(cbuffer,"Command    %02X    Set  PWM Period",SetPWMPer);
     printLine(0, cbuffer, ILI9340_WHITE, ILI9340_BLUE);
-    sprintf(cbuffer,"Byte1  %02X  Period MSB B15.B14.B13.B12.B11.B10",cPeriodMSBbits);
+    sprintf(cbuffer,"Byte1  %02X  Period MSB B11.B10.B9.B8.B7.B6",cPeriodMSBbits);
     printLine(2, cbuffer, ILI9340_WHITE, ILI9340_BLUE);
-    sprintf(cbuffer,"Byte2  %02X  Period MSB Bits B11.B10.B9.B8.B7.B6",cPeriodMLSBbits);
+    sprintf(cbuffer,"Byte2  %02X  Period LSB Bits B5.B4.B3.B2.B1.B0",cPeriodLSBbits);
     printLine(4, cbuffer, ILI9340_WHITE, ILI9340_BLUE);
-    sprintf(cbuffer,"Byte3  %02X  Period LSB Bits B5.B4.B3.B2.B1.B0",cPeriodLSBbits);
-    printLine(6, cbuffer, ILI9340_WHITE, ILI9340_BLUE); 
-    iPeriod=((cPeriodMSBbits & 0x3f)<<10)|((cPeriodMLSBbits & 0x1f)<<5)|cPeriodLSBbits;
-    sprintf(cbuffer,"Set PWM Period to %d",iPeriod);
+    iPeriod=(((cPeriodMSBbits & 0x1f)<<5)|cPeriodLSBbits)*40;
+    sprintf(cbuffer,"Set PWM Period to %d ms",iPeriod);
     printLine(8, cbuffer, ILI9340_WHITE, ILI9340_BLUE); 
     OpenTimer2(T2_ON | T2_SOURCE_INT | T2_PS_1_1, iPeriod);
     ConfigIntTimer2(T2_INT_ON | T2_INT_PRIOR_2); 
@@ -309,19 +316,16 @@ void vExec_Set_PWM_Per(char *cRecvData){
 void vExec_Start_PWM1(char *cRecvData){
     char cOnTime1MSBbits= cRecvData[0];
     char cOnTime1MLSBbits =cRecvData[1];
-    char cOnTime1LSBbits =cRecvData[2];
     int iPWM1ONPeriod;
     tft_fillScreen(ILI9340_BLACK);
     sprintf(cbuffer,"Command    %02X    Set  PWM 1",StartPWM1);
     printLine(0, cbuffer, ILI9340_WHITE, ILI9340_BLUE);
-    sprintf(cbuffer,"Byte1  %02X  On Time 1 MSB B15.B14.B13.B12.B11.B10",cOnTime1MSBbits);
+    sprintf(cbuffer,"Byte1  %02X  On Time 1 MSB B11.B10.B9.B8.B7.B6",cOnTime1MSBbits);
     printLine(2, cbuffer, ILI9340_WHITE, ILI9340_BLUE);
-    sprintf(cbuffer,"Byte2  %02X  On Time 1 MSB Bits B11.B10.B9.B8.B7.B6",cOnTime1MLSBbits);
+    sprintf(cbuffer,"Byte2  %02X  On Time 1 LSB Bits B5.B4.B3.B2.B1.B0",cOnTime1LSBbits);
     printLine(4, cbuffer, ILI9340_WHITE, ILI9340_BLUE);
-    sprintf(cbuffer,"Byte3  %02X  On Time 1 LSB Bits B5.B4.B3.B2.B1.B0",cOnTime1LSBbits);
-    printLine(6, cbuffer, ILI9340_WHITE, ILI9340_BLUE); 
-    iPWM1ONPeriod=((cOnTime1MSBbits & 0x3f)<<10)|((cOnTime1MLSBbits & 0x1f)<<5)|cOnTime1LSBbits;
-    sprintf(cbuffer,"Set PWM Period to %d",iPWM1ONPeriod);
+    iPWM1ONPeriod=(((cOnTime1MSBbits & 0x1f)<<5)|cOnTime1LSBbits)*40;
+    sprintf(cbuffer,"Set PWM Period to %d ms",iPWM1ONPeriod);
     printLine(8, cbuffer, ILI9340_WHITE, ILI9340_BLUE); 
     SetDCOC1PWM(iPWM1ONPeriod);
 
@@ -330,18 +334,15 @@ void vExec_Start_PWM1(char *cRecvData){
 void vExec_Start_PWM2(char *cRecvData){
     char cOnTime2MSBbits= cRecvData[0];
     char cOnTime2MLSBbits =cRecvData[1];
-    char cOnTime2LSBbits =cRecvData[2];
     int iPWM2ONPeriod;
     tft_fillScreen(ILI9340_BLACK);
     sprintf(cbuffer,"Command    %02X    Set  PWM 1",StartPWM2);
     printLine(0, cbuffer, ILI9340_WHITE, ILI9340_BLUE);
-    sprintf(cbuffer,"Byte1  %02X  On Time 1 MSB B15.B14.B13.B12.B11.B10",cOnTime2MSBbits);
+    sprintf(cbuffer,"Byte1  %02X  On Time 1 MSB B11.B10.B9.B8.B7.B6",cOnTime2MSBbits);
     printLine(2, cbuffer, ILI9340_WHITE, ILI9340_BLUE);
-    sprintf(cbuffer,"Byte2  %02X  On Time 1 MSB Bits B11.B10.B9.B8.B7.B6",cOnTime2MLSBbits);
+    sprintf(cbuffer,"Byte2  %02X  On Time 1 LSB B5.B4.B3.B2.B1.B0",cOnTime2LSBbits);
     printLine(4, cbuffer, ILI9340_WHITE, ILI9340_BLUE);
-    sprintf(cbuffer,"Byte3  %02X  On Time 1 LSB Bits B5.B4.B3.B2.B1.B0",cOnTime2LSBbits);
-    printLine(6, cbuffer, ILI9340_WHITE, ILI9340_BLUE); 
-    iPWM2ONPeriod=((cOnTime2MSBbits & 0x3f)<<10)|((cOnTime2MLSBbits & 0x1f)<<5)|cOnTime2LSBbits;
+    iPWM2ONPeriod=(((cOnTime2MLSBbits & 0x1f)<<5)|cOnTime2LSBbits)*40;
     sprintf(cbuffer,"Set PWM Period to %d",iPWM2ONPeriod);
     printLine(8, cbuffer, ILI9340_WHITE, ILI9340_BLUE); 
     SetDCOC4PWM(iPWM2ONPeriod); 
@@ -381,11 +382,11 @@ int iNumOfDataBytes(unsigned char cRecvChar){
                         
                         case(StartADC):     return 1;
                     
-                        case(SetPWMPer):    return 3;
+                        case(SetPWMPer):    return 2;
              
-                        case(StartPWM1):    return 3;
+                        case(StartPWM1):    return 2;
              
-                        case(StartPWM2):    return 3;
+                        case(StartPWM2):    return 2;
                         
                         case(ReadBuf):      return 3;
                         
@@ -450,73 +451,6 @@ static int iNumOfRecvBytes=0;
 static int iByteCount=0;
 static unsigned char cCommand;
 
-static PT_THREAD (protothread_uart_receive(struct pt *pt))
-{
-     PT_BEGIN(pt);
-   
-    while(1){
-      while(!UARTReceivedDataIsAvailable(UART2));
-       // PT_YIELD_UNTIL(pt, UARTReceivedDataIsAvailable(UART1));
-        cRecvChar = UARTGetDataByte(UART2);
-        //printf(cRecvChar);
-        
-        
-       /* while(!UARTTransmitterIsReady(UART2));
-        UARTSendDataByte(UART2,cRecvChar );
-          while(!UARTTransmitterIsReady(UART2));
-        UARTSendDataByte(UART2,RecvState );
-          while(!UARTTransmitterIsReady(UART2));
-        UARTSendDataByte(UART2,RecvStatus );
-            while(!UARTTransmitterIsReady(UART2));
-        UARTSendDataByte(UART2,'\n' );
-        */
-        if(RecvState==ACTIVE_WAIT_FOR_SOT && cRecvChar== 0xF0){
-            RecvState=ACTIVE_PARSE;
-            iCheckSumValid=0;
-            RecvStatus=NoERR;
-        }
-        
-        else if (RecvState==ACTIVE_WAIT_FOR_EOT && cRecvChar== EndOfTransmit){
-                if(RecvStatus==NoERR){
-                    vExecuteCommand(cCommand,cRecvData);
-                    vSendRespMsg(cCommand,cTranData);
-                }
-                else
-                    vSendErrorMsg(RecvStatus);
-            
-                RecvState=ACTIVE_WAIT_FOR_SOT;
-        }
-        
-        else if (RecvState==ACTIVE_CHECKSUM){
-                if(!CheckSum(cRecvData,iNumOfRecvBytes,cRecvChar))
-                        RecvStatus=CheckSumERR;
-               RecvState=ACTIVE_WAIT_FOR_EOT;
-                }
-        
-        else if(RecvState==ACTIVE_PARSE){
-                    cCommand=cRecvChar;
-                    iNumOfRecvBytes=iNumOfDataBytes(cCommand);
-                
-                if(iNumOfRecvBytes ==-1)
-                            RecvStatus=InvalidCmdERR;
-                            
-                else{
-                    
-                    for(iByteCount=0;iByteCount<iNumOfRecvBytes;iByteCount++){
-                            while(!UARTReceivedDataIsAvailable(UART2));
-                            cRecvData[iByteCount]=UARTGetDataByte(UART2);
-                        }
-                }
-                RecvState=ACTIVE_CHECKSUM;
-        }
-  
-    }
-      
-    PT_END(pt);
-
-    
-}
-
 
 static PT_THREAD (protothread_timer(struct pt *pt))
 {
@@ -562,6 +496,65 @@ void Variable_Initialize(void){
     iNumOfADCSamples[3]=0;
     RecvState= ACTIVE_WAIT_FOR_SOT;  
  
+}
+
+// handler for the DMA channel 1 interrupt
+void __ISR(_DMA1_VECTOR, IPL5SOFT) DmaHandler1(void)
+{
+
+	INTClearFlag(INT_SOURCE_DMA(DMA_CHANNEL1));	// release the interrupt in the INT controller, we're servicing int
+
+    int iByte=0;
+
+    do{
+            cRecvChar=ReceiveBuf[iByte++]; 
+            if(RecvState==ACTIVE_WAIT_FOR_SOT && cRecvChar== 0xF0){
+            RecvState=ACTIVE_PARSE;
+            iCheckSumValid=0;
+            RecvStatus=NoERR;
+        }
+        
+        else if (RecvState==ACTIVE_WAIT_FOR_EOT && cRecvChar== EndOfTransmit){
+                if(RecvStatus==NoERR){
+                    vExecuteCommand(cCommand,cRecvData);
+                    vSendRespMsg(cCommand,cTranData);
+                }
+                else
+                    vSendErrorMsg(RecvStatus);
+            
+                RecvState=ACTIVE_WAIT_FOR_SOT;
+        }
+        
+        else if (RecvState==ACTIVE_CHECKSUM){
+                if(!CheckSum(cRecvData,iNumOfRecvBytes,cRecvChar))
+                        RecvStatus=CheckSumERR;
+               RecvState=ACTIVE_WAIT_FOR_EOT;
+                }
+        
+        else if(RecvState==ACTIVE_PARSE){
+                    cCommand=cRecvChar;
+                    iNumOfRecvBytes=iNumOfDataBytes(cCommand);
+                
+                if(iNumOfRecvBytes ==-1)
+                            RecvStatus=InvalidCmdERR;
+                            
+                else{
+                    
+                    for(iByteCount=0;iByteCount<iNumOfRecvBytes;iByteCount++){
+                            cRecvData[iByteCount]=ReceiveBuf[iByte++]; 
+                        }
+                }
+                RecvState=ACTIVE_CHECKSUM;
+        } 
+        
+        
+    }while(cRecvChar!= EndOfTransmit);
+    
+	DmaChnClrEvFlags(DMA_CHANNEL1, DMA_EV_BLOCK_DONE);
+    INTEnable(INT_SOURCE_DMA(DMA_CHANNEL1), INT_ENABLED);		// enable the chn interrupt in the INT controller
+
+	// enable the chn
+	DmaChnEnable(DMA_CHANNEL1);
 }
 
 
@@ -647,9 +640,38 @@ EnableADC10(); // Enable the ADC
     iNumSendChars=0;
     while(!UARTTransmitterIsReady(UART2));
         UARTSendDataByte(UART2, '\n');
+    
+    
+    
+    DmaChnOpen(DMA_CHANNEL1, DMA_CHN_PRI2, DMA_OPEN_MATCH);
+
+	DmaChnSetMatchPattern(DMA_CHANNEL1, EndOfTransmit);	// set \r as ending character
+
+	// set the events: we want the UART2 rx interrupt to start our transfer
+	// also we want to enable the pattern match: transfer stops upon detection of EOT
+	DmaChnSetEventControl(DMA_CHANNEL1, DMA_EV_START_IRQ_EN|DMA_EV_MATCH_EN|DMA_EV_START_IRQ(_UART2_RX_IRQ));
+
+	// set the transfer source and dest addresses, source and dest sizes and the cell size
+	DmaChnSetTxfer(DMA_CHANNEL1, (void*)&U2RXREG, ReceiveBuf, 1, 256, 1);
+
+	DmaChnSetEvEnableFlags(DMA_CHANNEL1, DMA_EV_BLOCK_DONE);		// enable the transfer done interrupt: pattern match or all the characters transferred
+
+	// enable system wide multi vectored interrupts
+	INTConfigureSystem(INT_SYSTEM_CONFIG_MULT_VECTOR);
+	INTEnableInterrupts();
+
+	INTSetVectorPriority(INT_VECTOR_DMA(DMA_CHANNEL1), INT_PRIORITY_LEVEL_5);		// set INT controller priority
+	INTSetVectorSubPriority(INT_VECTOR_DMA(DMA_CHANNEL1), INT_SUB_PRIORITY_LEVEL_3);		// set INT controller sub-priority
+
+	INTEnable(INT_SOURCE_DMA(DMA_CHANNEL1), INT_ENABLED);		// enable the chn interrupt in the INT controller
+
+	// enable the chn
+	DmaChnEnable(DMA_CHANNEL1);
+    
+    
+    
 while (1){
       PT_SCHEDULE(protothread_timer(&pt_timer));
-      PT_SCHEDULE(protothread_uart_receive(&pt_uart_receive));
 
   }
     
